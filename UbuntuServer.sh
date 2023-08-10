5459ed58c1175ca9fdc674a736d334a6961ba0b1
DarkZoneSD <113187443+DarkZoneSD@users.noreply.github.com>
2023-08-10 07:31:09 +0200

    Update UbuntuServer.sh

--- UbuntuServer.sh
+++ UbuntuServer.sh
@@ -1,5 +1,5 @@
 #Install updates
-sudo apt-get update -y && sudo apt-get upgrade
+sudo apt-get update -y && sudo apt-get upgrade -y
 
 #Install Btop and Net-Tools and NCDU
 sudo apt install net-tools
@@ -16,16 +16,14 @@ sudo journalctl --vacuum-size=50M
 #Cleanup unused Packages
 sudo apt autoremove -y
 
-#Install Dockerand Docker-Compose
-sudo apt-get install ca-certificates curl gnupg
-sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
-echo \
-"deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-"$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
+#Install Docke and Docker-Compose
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt-cache policy docker-ce
+sudo apt install docker-ce
+sudo systemctl status docker
+
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
