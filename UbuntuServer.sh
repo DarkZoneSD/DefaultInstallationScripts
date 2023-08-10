1530488da5217c43eb7350897beac2818d9fa5a5
DarkZoneSD <113187443+DarkZoneSD@users.noreply.github.com>
2023-08-10 07:32:15 +0200

    Update UbuntuServer.sh

--- UbuntuServer.sh
+++ UbuntuServer.sh
@@ -16,7 +16,7 @@ sudo journalctl --vacuum-size=50M
 #Cleanup unused Packages
 sudo apt autoremove -y
 
-#Install Docke and Docker-Compose
+#Install Docker and Docker-Compose
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
